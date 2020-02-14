7239d4962e8bb826db92b0709a9103ba1e82cd92
Felipe Velazquez <lfvr_cabos@hotmail.com>
2020-02-14 11:29:03 -0600

    V1.1

--- scripts/Data_recolect.py
+++ scripts/Data_recolect.py
@@ -39,42 +39,29 @@ Giro = 0.0
 servo.setTarget(0, 6000)
 servo.setTarget(1, 6000)
 
+vary = 6000
 
-while True:
+while Joy.Back() != 1:
     check, frame = cap.read()
     frame2 = cv2.resize(frame, (640, 480), interpolation = cv2.INTER_AREA)
     cv2.imshow('Webcam Video', frame2)
 
     data_X = joy.leftX() # Izquierda y Derecha
     data_Y = joy.leftY() #Arriba y abajo o adelante y atras
-    servo.setTarget(1, 6000)
+    
+    gradosX = (data_X * 3000) + 6000
+    servo.setTarget(0, int(gradosX))
 
-    if data_X >= 0.5:
-    	aint = aint + 1
-    	Derecho = 'Derecho/derecho' + str(aint) + '.jpg'
-    	cv2.imwrite(filename=Derecho, img=frame2)
-    	print('Guardando Adelante: ' + str(aint))
-    	servo.setTarget(1, 6100)
-        
-    elif data_X <= -0.5: # press Q for save image has Coca-cola
-        atint = atint + 1
-    	Atras = 'Atras/atras' + str(atint) + '.jpg'
-    	cv2.imwrite(filename=Atras, img=frame2)
-    	print('Guardando Atras: ' + str(atint))
-    	servo.setTarget(1, 5900)
+    gradosY = (data_Y * 3000) + 6000
+    servo.setTarget(1, int(gradosY))
+    print("Grados: " + str(int(gradosY)) + " Joy: " + str(data_Y))
+    if data_Y >= 0.2:
+    	vary = 6150
+    elif data_Y < 0:
+    	vary = 5850
+    else:
+    	vary = 6000
 
-    elif data_Y >= 0.5: # press W for save image has termo
-        izint = izint + 1
-    	Izquierda = 'Izquierda/izquierda' + str(izint) + '.jpg'
-    	cv2.imwrite(filename=Izquierda, img=frame2)
-    	print('Guardando Izquierda: ' + str(izint))
-    	Giro = Giro - izint * 10
-    	servo.setTarget(0, 6000 - Giro)
+    
 
-    elif data_Y <= -0.5: # press E for save image has agua
-        derint = derint + 1
-    	Derecha = 'Derecha/derecha' + str(derint) + '.jpg'
-    	cv2.imwrite(filename=Derecha, img=frame2)
-    	print('Guardando Derecha: ' + str(derint))
-    	Giro = Giro + izint * 10
-    	servo.setTarget(0, 6000 + Giro)
+Joy.close()
