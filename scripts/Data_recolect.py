29e6efd7ca9337ff5aee3066338a8833a61d2b6b
Felipe Velazquez <lfvr_cabos@hotmail.com>
2020-02-15 11:35:36 -0600

    V1.3 Serial COM
    Data control over serial communication

--- scripts/Data_recolect.py
+++ scripts/Data_recolect.py
@@ -7,17 +7,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-import xbox
-import maestro 
+#import xbox
+import serial
+import maestro
+import sklearn
+import skimage
 from skimage.transform import resize
 
 
-cap = cv2.VideoCapture(0)
-joy = xbox.Joystick()
+#cap = cv2.VideoCapture(0)
+#joy = xbox.Joystick()
 servo = maestro.Controller()
 
 servo.setTarget(0, 6000)
 servo.setTarget(1, 6000)
+topic = "/zed2/zed_node/stereo_raw/image_raw_color"
 
 count = 0
 
@@ -27,45 +31,61 @@ Imagen = []
 Velocidad = []
 Direccion = []
 
-while Joy.Back() != 1:
-    check, frame = cap.read()
-    frame2 = cv2.resize(frame, (640, 480), interpolation = cv2.INTER_AREA)
-    cv2.imshow('Webcam Video', frame2)
+ser = serial.Serial('/dev/ttyACM0')
 
-    data_X = joy.leftX() # Izquierda y Derecha
-    data_Y = joy.leftY() #Arriba y abajo o adelante y atras
-    
-    gradosX = (data_X * 3000) + 6000
-    servo.setTarget(0, int(gradosX))
+def callback(data):
+	if Joy.Back() != 1:
+	    #check, frame = cap.read()
+	    #frame2 = cv2.resize(frame, (640, 480), interpolation = cv2.INTER_AREA)
+	    #cv2.imshow('Webcam Video', frame2)
+	    bridge = CvBridge()
+	    frame = bridge.imgmsg_to_cv2(data, "bgr8")
 
-    gradosY = (data_Y * 3000) + 6000
-    servo.setTarget(1, int(gradosY))
+	    #data_X = joy.leftX() # Izquierda y Derecha
+	    #data_Y = joy.rightY() #Arriba y abajo o adelante y atras
+	    ser.write(b'1\n')
+	    data_XY = ser.readline()
 
-    print("Grados: " + str(int(gradosY)) + " Joy: " + str(data_Y))
+	    (data_X, data_Y) = data_XY.split(',')
+	    
+	    gradosX = (float(data_X) * 3000) + 6000
+	    servo.setTarget(0, int(gradosX))
 
-    if data_Y >= 0.2:
-    	vary = 6150
-    elif data_Y < 0:
-    	vary = 5850
-    else:
-    	vary = 6000
+	    gradosY = (float(data_Y) * 3000) + 6000
+	    servo.setTarget(1, int(gradosY))
 
-    count = count + 1
+	    print("Grados: " + str(int(gradosY)) + " Joy: " + str(data_Y))
 
-    if count >= 10 and (data_Y != 0 or data_X != 0):
-    	rez_img = skimage.transform.resize(frame, (150, 150, 3),mode='constant',anti_aliasing=True)
-    	img_arr = np.asarray(rez_img)
-    	Imagen.append(img_arr)
-    	Velocidad.append(gradosY)
-    	Direccion.append(gradosX)
-    	count = 0
+	    if data_Y >= 0.2:
+	    	vary = 6150
+	    elif data_Y < 0:
+	    	vary = 5850
+	    else:
+	    	vary = 6000
 
-Imagen = np.asarray(Imagen)
-Velocidad = np.asarray(Velocidad)
-Direccion = np.asarray(Direccion)
+	    count = count + 1
 
-np.save('Imagenes.npy', Imagenes)
-np.save('Velocidad.npy', Velocidad)
-np.save('Direccion.npy', Direccion)
+	    if count >= 10 and (data_Y != 0 or data_X != 0):
+	    	rez_img = skimage.transform.resize(frame, (150, 150, 3),mode='constant',anti_aliasing=True)
+	    	img_arr = np.asarray(rez_img)
+	    	Imagen.append(img_arr)
+	    	Velocidad.append(gradosY)
+	    	Direccion.append(gradosX)
+	    	count = 0
+	else:
+		Imagen = np.asarray(Imagen)
+		Velocidad = np.asarray(Velocidad)
+		Direccion = np.asarray(Direccion)
+		np.save('Imagenes.npy', Imagenes)
+		np.save('Velocidad.npy', Velocidad)
+		np.save('Direccion.npy', Direccion)	
+		Joy.close()
 
-Joy.close()
+def listener():
+	rospy.init_node('ai_dataCreator', anonymous=True)
+    rospy.Subscriber(topic, Image, callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
